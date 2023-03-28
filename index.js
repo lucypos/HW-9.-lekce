7563b76228a99695b83a430275811aa23005217e
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-03-28 15:02:04 +0200

    HW 1+2

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 //Auto
-const posun = () => {
-    document.getElementById('auto').addEventListener('onkeydown')
-    if 
+
+document.addEventListener('keydown', (event) => {
+     if (event.key === 'ArrowRight'){ 
+       document.getElementById('auto').style.marginLeft = '200px';
+    } 
+     else if (event.key === 'ArrowLeft') document.getElementById('auto').style.marginLeft = '0px';
+    })
+
+
+//Přihlášení
+const user = {
+	email: 'bartolomej.rozumbrada@gmail.com',
+	password: 'vimzenicnevim',
+	name: 'Bartoloměj',
 }
+
+document.querySelector('#login-form').addEventListener('submit', (event) => {
+	event.preventDefault()
+
+	const emailInput = document.querySelector('#email-input')
+	const passwordInput = document.querySelector('#password-input')
+	const container = document.querySelector('.container')
+	const status = document.querySelector('#status')
+
+	if (
+		emailInput.value === user.email &&
+		passwordInput.value === user.password
+	) {
+		container.innerHTML = `<h1>Přihlášený uživatel: ${user.name}</h1>`
+	} else {
+		status.textContent = 'Neplatné přihlašovací údaje'
+		passwordInput.value = ''
+	}
+})
+
+//Newsletter, kontrola
+
